0538e21d69c2e2d79c3cb3f6e3b8c07c420ef908
zhMargDev <155582703+zhMargDev@users.noreply.github.com>
2024-10-07 20:44:45 +0400

    Update config.py

--- configs/config.py
+++ configs/config.py
@@ -1,46 +1,13 @@
 PRICE_DIFFERENCE = 99
-#6915638527:AAFxtX7zL_6xsfr8Z7e_Jkw_ofuj8djgkL4
 ru_rate = 1
 en_rate = 1
 RU_GROUPS = {
     '1' : {
-        'BOT' : "6915638527:AAFxtX7zL_6xsfr8Z7e_Jkw_ofuj8djgkL4",
+        'BOT' : "bot_id",
         "CHANNELS" : {
-            'LITE' : '-1002248049523',
-            'PREMIUM' : '-1002167543348',
-            'VIP': '-1002193936418'
-        },
-        "REGION_DEFAULT" : "Def" ,
-        "LAVARAGE_DEFAULT" : "Def",
-        "STOP_DEFAULT" : "Def",
-        "LITE_SIGNAL_COUNT" : 2 , 
-        "PREMIUM_SIGNAL_COUNT" : 5 , 
-        "LITE_SIGNAL_PROBELS" : 4,
-        "PREMIUM_SIGNAL_PROBELS" : 2
-
-    },
-    '2' : {
-        'BOT' : "6915638527:AAFxtX7zL_6xsfr8Z7e_Jkw_ofuj8djgkL4",
-        "CHANNELS" : {
-            'LITE' : '-1002248049523',
-            'PREMIUM' : '-1002167543348',
-            'VIP': '-1002193936418'
-        },
-        "REGION_DEFAULT" : "Def" ,
-        "LAVARAGE_DEFAULT" : "Def",
-        "STOP_DEFAULT" : "Def",
-        "LITE_SIGNAL_COUNT" : 2 , 
-        "PREMIUM_SIGNAL_COUNT" : 5 , 
-        "LITE_SIGNAL_PROBELS" : 4,
-        "PREMIUM_SIGNAL_PROBELS" : 2
-
-    },
-    '3' : {
-        'BOT' : "6915638527:AAFxtX7zL_6xsfr8Z7e_Jkw_ofuj8djgkL4",
-        "CHANNELS" : {
-            'LITE' : '-1002248049523',
-            'PREMIUM' : '-1002167543348',
-            'VIP': '-1002193936418'
+            'LITE' : 'channgel_key',
+            'PREMIUM' : 'channgel_key',
+            'VIP': 'channgel_key'
         },
         "REGION_DEFAULT" : "Def" ,
         "LAVARAGE_DEFAULT" : "Def",
@@ -55,11 +22,11 @@ RU_GROUPS = {
 
 EN_GROUPS = {
     '1' : {
-              'BOT' : "6915638527:AAFxtX7zL_6xsfr8Z7e_Jkw_ofuj8djgkL4",
+        'BOT' : "bot_id",
         "CHANNELS" : {
-            'LITE' : 241421,
-            'PREMIUM' : 214124,
-            'VIP': 214124 
+            'LITE' : 'channgel_key',
+            'PREMIUM' : 'channgel_key',
+            'VIP': 'channgel_key'
         },
         "REGION_DEFAULT" : "Def" ,
         "LAVARAGE_DEFAULT" : "Def",
@@ -68,50 +35,5 @@ EN_GROUPS = {
         "PREMIUM_SIGNAL_COUNT" : 5 , 
         "LITE_SIGNAL_PROBELS" : 4,
         "PREMIUM_SIGNAL_PROBELS" : 2
-    },
-    '2' : {
-               'BOT' : "6915638527:AAFxtX7zL_6xsfr8Z7e_Jkw_ofuj8djgkL4",
-        "CHANNELS" : {
-            'LITE' : 241421,
-            'PREMIUM' : 214124,
-            'VIP': 214124 
-        },
-        "REGION_DEFAULT" : "Def" ,
-        "LAVARAGE_DEFAULT" : "Def",
-        "STOP_DEFAULT" : "Def",
-        "LITE_SIGNAL_COUNT" : 2 , 
-        "PREMIUM_SIGNAL_COUNT" : 5 , 
-        "LITE_SIGNAL_PROBELS" : 4,
-        "PREMIUM_SIGNAL_PROBELS" : 2
-    },
-    '3' : {
-               'BOT' : "6915638527:AAFxtX7zL_6xsfr8Z7e_Jkw_ofuj8djgkL4",
-        "CHANNELS" : {
-            'LITE' : 241421,
-            'PREMIUM' : 214124,
-            'VIP': 214124 
-        },
-        "REGION_DEFAULT" : "Def" ,
-        "LAVARAGE_DEFAULT" : "Def",
-        "STOP_DEFAULT" : "Def",
-        "LITE_SIGNAL_COUNT" : 2 , 
-        "PREMIUM_SIGNAL_COUNT" : 5 , 
-        "LITE_SIGNAL_PROBELS" : 4,
-        "PREMIUM_SIGNAL_PROBELS" : 2
-    },
-    '4' : {
-               'BOT' : "6915638527:AAFxtX7zL_6xsfr8Z7e_Jkw_ofuj8djgkL4",
-        "CHANNELS" : {
-            'LITE' : 241421,
-            'PREMIUM' : 214124,
-            'VIP': 214124 
-        },
-        "REGION_DEFAULT" : "Def" ,
-        "LAVARAGE_DEFAULT" : "Def",
-        "STOP_DEFAULT" : "Def",
-        "LITE_SIGNAL_COUNT" : 2 , 
-        "PREMIUM_SIGNAL_COUNT" : 5 , 
-        "LITE_SIGNAL_PROBELS" : 4,
-        "PREMIUM_SIGNAL_PROBELS" : 2
-    },
+    }
 }
